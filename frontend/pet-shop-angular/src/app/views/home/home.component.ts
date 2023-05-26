aa2067f7d6ebdf2b1e0e9b51ec3df1c113d6036c
HHogasen <hallgeirhogasen@hotmail.com>
2023-05-27 00:36:56 +0200

    så fixes
    ålø da ser held decent ut

--- frontend/pet-shop-angular/src/app/views/home/home.component.ts
+++ frontend/pet-shop-angular/src/app/views/home/home.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/autentication.service';
+/* den liker ikke denne linja, men det er dette som gjør at det funker, den vil at jeg skal skrive import { AuthenticationService } from 'src/app/services/autentication.service'; men det funker ikke */
+
 import { AboutUsComponent } from '../about-us/about-us.component';
 import { ItemAttributes, ItemsService } from 'src/app/services/items.service';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit {
   activeIndex: number = 0;
+  activeIndex2:number = 0;
   intervalId: any;
+  intervalId2:any;
 
   images = [
-    { src: 'assets/somePictures/dog-collar.png', alt: 'epic collar', active: true, price: 10.99 },
-    { src: 'assets/somePictures/kanis-hus.png', alt: 'Image 2', active: true, price: 15.99 },
-    { src: 'assets/somePictures/dog-collar.png', alt: 'Image 3', active: true, price: 12.99 },
+    { src: 'assets/somePictures/dog-collar.png', alt: 'epic collar', active: false, price: 10.99 },
+    { src: 'assets/somePictures/kanis-hus.png', alt: 'Image 2', active: false, price: 15.99 },
+    { src: 'assets/somePictures/dog-collar.png', alt: 'Image 3', active: false, price: 12.99 },
     { src: 'assets/somePictures/kanis-hus.png', alt: 'Image 5', active: true, price: 9.99 },
     { src: 'assets/somePictures/skildpadde-med-hatt.png', alt: 'nice hat', active: true, price: 8.99 },
     { src: 'assets/somePictures/kanis-hus.png', alt: 'dream house', active: true, price: 10.99 }
@@ -32,13 +35,13 @@ export class HomeComponent implements OnInit, OnDestroy {
     {
       src: 'assets/somePictures/kanis-hus.png',
       alt: 'Image 2',
-      active: false,
+      active: true,
       price: 15
     },
     {
-      src: 'assets/somePictures/kanis-hus.png',
-      alt: 'Image 3',
-      active: true,
+      src: 'assets/somePictures/dog-collar.png',
+      alt: 'Image gay',
+      active: false,
       price: 15
     }
   ];
@@ -76,7 +79,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.activeIndex = 0; // Set the initial active index to 0
     this.intervalId = setInterval(() => {
       this.switchToNextImage();
-    }, 5000);
+    }, 4000);
   }
 
   stopImageSwitching() {
@@ -96,33 +99,31 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.activeIndex = (this.activeIndex - 1 + totalImages) % totalImages;
     this.images[this.activeIndex].active = true;
   }
-
   startImageSwitching2() {
-    this.activeIndex = 0; // Set the initial active index to 0
-    this.intervalId = setInterval(() => {
+    this.activeIndex2 = 0; // Set the initial active index to 0
+    this.intervalId2 = setInterval(() => {
       this.switchToNextImage2();
-    }, 5000);
+    }, 6000);
   }
 
   stopImageSwitching2() {
-    clearInterval(this.intervalId);
+    clearInterval(this.intervalId2);
   }
-
   switchToNextImage2() {
-    const totalImages = this.images2.length;
+    const totalImages2 = this.images2.length;
     this.images2.forEach((image) => {
       image.active = false;
     });
-    this.activeIndex = (this.activeIndex + 1) % totalImages;
-    this.images2[this.activeIndex].active = true;
+    this.activeIndex2 = (this.activeIndex2 + 1) % totalImages2;
+    this.images2[this.activeIndex2].active = true;
   }
 
   switchToPreviousImage2() {
-    const totalImages = this.images2.length;
+    const totalImages2 = this.images2.length;
     this.images2.forEach((image) => {
       image.active = false;
     });
-    this.activeIndex = (this.activeIndex - 1 + totalImages) % totalImages;
-    this.images2[this.activeIndex].active = true;
+    this.activeIndex2 = (this.activeIndex2 - 1 + totalImages2) % totalImages2;
+    this.images2[this.activeIndex2].active = true;
   }
 }
