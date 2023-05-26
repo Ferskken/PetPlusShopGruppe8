0a5eda6597bcf27089908ecba309ce159474ad22
HHogasen <hallgeirhogasen@hotmail.com>
2023-05-26 15:46:33 +0200

    home
    home ser noe bedre ut

--- frontend/pet-shop-angular/src/app/views/home/home.component.ts
+++ frontend/pet-shop-angular/src/app/views/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
 import { AuthenticationService } from 'src/app/services/autentication.service';
 
@@ -7,28 +7,22 @@ import { AuthenticationService } from 'src/app/services/autentication.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+
 export class HomeComponent {
-
-  constructor(private authenticationService: AuthenticationService) {
-    console.log(this.authenticationService.userValue);
-    if (Object.keys(this.authenticationService.userValue).length == 0) {
-      this.authenticationService.login("guest@petshop.com", "").pipe(first()).subscribe();
-    }
-  }
-
   images = [
-    { src: 'assets/somePictures/dog-collar.png', alt: 'Image 1', active: true },
-    { src: 'assets/somePictures/kanis-hus.png', alt: 'Image 2', active: true },
-    { src: 'assets/somePictures/dog-collar.png', alt: 'Image 3', active: false },
-    { src: 'assets/somePictures/skildpadde-med-hatt.png', alt: 'Image 4', active: false },
-    { src: 'assets/somePictures/kanis-hus.png', alt: 'Image 5', active: false }
+    { src: 'assets/somePictures/dog-collar.png', alt: 'Image 1', active: true, price: 10.99 },
+    { src: 'assets/somePictures/kanis-hus.png', alt: 'Image 2', active: true, price: 15.99 },
+    { src: 'assets/somePictures/dog-collar.png', alt: 'Image 3', active: false, price: 12.99 },
+    { src: 'assets/somePictures/skildpadde-med-hatt.png', alt: 'Image 4', active: false, price: 8.99 },
+    { src: 'assets/somePictures/kanis-hus.png', alt: 'Image 5', active: false, price: 9.99 }
   ];
+  
+
+  activeIndex = 0;
 
   showNextImage() {
-    const currentIndex = this.images.findIndex(image => image.active);
-    this.images[currentIndex].active = false;
-    const nextIndex = (currentIndex + 1) % this.images.length;
-    this.images[nextIndex].active = true;
+    this.activeIndex = (this.activeIndex + 1) % this.images.length;
   }
 
   ngOnInit() {
